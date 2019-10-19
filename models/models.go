fa2aab3ca9f5697034773a62538992d0a3aab835
idir <idirall22@gmail.com>
2019-10-19 20:00:28 +0100

    added timeline model

--- models/models.go
+++ models/models.go
@@ -1,13 +1,27 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"github.com/idirall22/post/models"
+)
 
 // Group structure
 type Group struct {
-	ID        int64      `json:"id"`
-	Name      string     `json:"name"`
-	AdminID   int64      `json:"admin_id"`
-	UsersIDs  []int64    `json:"users_ids"`
-	CreatedAt time.Time  `json:"created_at"`
-	DeletedAt *time.Time `json:"deleted_at"`
+	ID         int64      `json:"id"`
+	Name       string     `json:"name"`
+	AdminID    int64      `json:"admin_id"`
+	TimelineID int64      `json:"timeline_id"`
+	UsersIDs   []int64    `json:"users_ids"`
+	CreatedAt  time.Time  `json:"created_at"`
+	DeletedAt  *time.Time `json:"deleted_at"`
+}
+
+// Timeline structure
+type Timeline struct {
+	ID        int64         `json:"id"`
+	GroupID   int64         `json:"group_id"`
+	Posts     []models.Post `json:"posts"`
+	CreatedAt time.Time     `json:"created_at"`
+	DeletedAt *time.Time    `json:"deleted_at"`
 }
