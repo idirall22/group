3c68ac4968e67f80973edefad8a9e8bd19d17016
idir <idirall22@gmail.com>
2019-10-19 20:08:45 +0100

    update provider interface

--- provider.go
+++ provider.go
@@ -8,10 +8,10 @@ import (
 
 // Provider interface
 type Provider interface {
-	New(ctx context.Context, userID int64, name string) (*models.Group, error)
+	New(ctx context.Context, userID int64, name string) (int64, error)
 	Get(ctx context.Context, id int64, name string) (*models.Group, error)
 	List(ctx context.Context, offset, limit int) ([]*models.Group, error)
-	Update(ctx context.Context, id int64, name string) (*models.Group, error)
+	Update(ctx context.Context, id int64, name string) (int64, error)
 	Delete(ctx context.Context, id, userID int64) error
 	Join(ctx context.Context, userID, groupID int64) (int64, error)
 	Leave(ctx context.Context, userID, groupID int64) (int64, error)
