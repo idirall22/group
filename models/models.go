c043a6be0d2b0cced231d284006ad177bf3876ee
idir <idirall22@gmail.com>
2019-10-22 18:36:29 +0100

    updated group model

--- models/models.go
+++ models/models.go
@@ -8,19 +8,10 @@ import (
 
 // Group structure
 type Group struct {
-	ID         int64      `json:"id"`
-	Name       string     `json:"name"`
-	AdminID    int64      `json:"admin_id"`
-	TimelineID int64      `json:"timeline_id"`
-	UsersIDs   []int64    `json:"users_ids"`
-	CreatedAt  time.Time  `json:"created_at"`
-	DeletedAt  *time.Time `json:"deleted_at"`
-}
-
-// Timeline structure
-type Timeline struct {
 	ID        int64         `json:"id"`
-	GroupID   int64         `json:"group_id"`
+	Name      string        `json:"name"`
+	AdminID   int64         `json:"admin_id"`
+	UsersIDs  []int64       `json:"users_ids"`
 	Posts     []models.Post `json:"posts"`
 	CreatedAt time.Time     `json:"created_at"`
 	DeletedAt *time.Time    `json:"deleted_at"`
