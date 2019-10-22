1ac7ae5fbb1e2c5da2c87ef461391462e955acc7
idir <idirall22@gmail.com>
2019-10-22 20:00:17 +0100

    added service handlers

---
+++ handlers.go
@@ -0,0 +1,38 @@
+package group
+
+import "net/http"
+
+// AddGroupHandler add a new group
+func (s *Service) AddGroupHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// GetGroupHandler get a group
+func (s *Service) GetGroupHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// ListGroupsHandler list groups
+func (s *Service) ListGroupsHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// UpdateGroupHandler update a group
+func (s *Service) UpdateGroupHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// DeleteGroupHandler delete a group
+func (s *Service) DeleteGroupHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// JoinGroupHandler join a group
+func (s *Service) JoinGroupHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// LeaveGroupHandler leave a group
+func (s *Service) LeaveGroupHandler(w http.ResponseWriter, r *http.Request) {
+
+}
