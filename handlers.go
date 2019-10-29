81353763a281820616df0eda96bf6be52223c661
idir <idirall22@gmail.com>
2019-10-29 18:10:23 +0100

    Fixed GetURLID func

--- handlers.go
+++ handlers.go
@@ -38,7 +38,7 @@ func (s *Service) AddGroupHandler(w http.ResponseWriter, r *http.Request) {
 // GetGroupHandler get a group
 func (s *Service) GetGroupHandler(w http.ResponseWriter, r *http.Request) {
 
-	id, err := utilities.GetURLID(r)
+	id, err := utilities.GetURLID(r, "")
 
 	if err != nil {
 		return
@@ -86,7 +86,7 @@ func (s *Service) ListGroupsHandler(w http.ResponseWriter, r *http.Request) {
 // UpdateGroupHandler update a group
 func (s *Service) UpdateGroupHandler(w http.ResponseWriter, r *http.Request) {
 
-	id, err := utilities.GetURLID(r)
+	id, err := utilities.GetURLID(r, "")
 
 	if err != nil {
 		return
@@ -113,7 +113,7 @@ func (s *Service) UpdateGroupHandler(w http.ResponseWriter, r *http.Request) {
 // DeleteGroupHandler delete a group
 func (s *Service) DeleteGroupHandler(w http.ResponseWriter, r *http.Request) {
 
-	id, err := utilities.GetURLID(r)
+	id, err := utilities.GetURLID(r, "")
 
 	if err != nil {
 		return
@@ -135,7 +135,7 @@ func (s *Service) DeleteGroupHandler(w http.ResponseWriter, r *http.Request) {
 // JoinGroupHandler join a group
 func (s *Service) JoinGroupHandler(w http.ResponseWriter, r *http.Request) {
 
-	id, err := utilities.GetURLID(r)
+	id, err := utilities.GetURLID(r, "")
 
 	if err != nil {
 		return
@@ -157,7 +157,7 @@ func (s *Service) JoinGroupHandler(w http.ResponseWriter, r *http.Request) {
 // LeaveGroupHandler leave a group
 func (s *Service) LeaveGroupHandler(w http.ResponseWriter, r *http.Request) {
 
-	id, err := utilities.GetURLID(r)
+	id, err := utilities.GetURLID(r, "")
 
 	if err != nil {
 		return
